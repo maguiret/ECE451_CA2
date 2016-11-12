573c11bebaca6a0d079e546de3652ac70e8f9fb4
N0X <tmaguire1854@gmail.com>
2016-11-12 15:31:51 -0800

    adding stuff

---
+++ ComputerAssignement2.m
@@ -0,0 +1,16 @@
+% clc 
+% clear
+
+% must run simulink before each problem and it's parameters
+%1b)
+plot(simout.Time, simout.Data);
+3-max(simout.Data)
+
+%1d)
+plot(simout.Time, simout.Data);
+0-min(simout.Data)
+
+
+%1f)
+plot(simout.Time, simout.Data);
+3 - max(simout.Data);
