7c12b06dff8fc4b2de3c65a3db535490bf2c5b12
maguiret <tmaguire1854@gmail.com>
2016-11-12 20:38:43 -0800

    finished problem 1

--- ComputerAssignement2.m
+++ ComputerAssignement2.m
@@ -1,16 +1,22 @@
-% clc 
-% clear
+clc 
+clear
 
-% must run simulink before each problem and it's parameters
-%1b)
-plot(simout.Time, simout.Data);
-3-max(simout.Data)
+% % must run simulink before each problem and it's parameters
+% %1b)
+% plot(simout.Time, simout.Data);
+% 3-max(simout.Data)
+% 
+% %1d)
+% plot(simout.Time, simout.Data);
+% 0-min(simout.Data)
+% 
+% 
+% %1f)
+% plot(simout.Time, simout.Data);
+% 3 - max(simout.Data);
 
-%1d)
-plot(simout.Time, simout.Data);
-0-min(simout.Data)
+%2)
 
 
-%1f)
-plot(simout.Time, simout.Data);
-3 - max(simout.Data);
+    
+pause
